4f9495a7e7ea518e3c3fdd9b1194b75894b54f54
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2020-12-22 22:07:20 +0100

    Push code

---
+++ ManagedSDL2/SDL.cs
@@ -0,0 +1,29 @@
+﻿using static Native.SDL;
+
+namespace ManagedSDL2
+{
+	public enum SDLInitFlags : uint
+	{
+		Timer = SDL_INIT_TIMER,
+		Audio = SDL_INIT_AUDIO,
+		Video = SDL_INIT_VIDEO,
+		Joystick = SDL_INIT_JOYSTICK,
+		Haptic = SDL_INIT_HAPTIC,
+		GameController = SDL_INIT_GAMECONTROLLER,
+		Events = SDL_INIT_EVENTS,
+		Sensors = SDL_INIT_SENSOR,
+		Everything = SDL_INIT_EVERYTHING
+	}
+
+	public static class SDL
+	{
+		public static int WindowposUndefined => SDL_WINDOWPOS_UNDEFINED;
+		public static int WindowposCentered => SDL_WINDOWPOS_CENTERED;
+
+		public static void Init(SDLInitFlags flags)
+		{
+			CustomDllImportResolver.Enable();
+			SDL_Init((uint)flags);
+		}
+	}
+}
---
+++ ManagedSDL2/SDLWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static Native.SDL;
+using static Native.SDL.SDL_WindowFlags;
+
+namespace ManagedSDL2
+{
+	public class SDLWindow : IDisposable
+	{
+		IntPtr sdlWindowPtr;
+
+		public SDLWindow(string title, int x, int y, int w, int h, bool shown = true)
+		{
+			sdlWindowPtr = SDL_CreateWindow(title, x, y, w, h, shown ? SDL_WINDOW_SHOWN : SDL_WINDOW_HIDDEN);
+		}
+
+		~SDLWindow() => Dispose();
+
+		bool disposed = false;
+
+		public void Dispose()
+		{
+			if (disposed)
+				return;
+
+			GC.SuppressFinalize(this);
+			SDL_DestroyWindow(sdlWindowPtr);
+
+			disposed = true;
+		}
+	}
+}
